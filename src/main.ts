aa5e05ff996d5f134afa2adfc0b2d49fefa5cdcd
m.malmir <mahsa.malmir769@gmail.com>
2024-10-13 17:50:27 +0330

    create temp electron config

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, dialog } from "electron";
 import * as path from "path";
 
 function createWindow() {
@@ -6,16 +6,78 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     height: 600,
     webPreferences: {
+      nodeIntegration: true,
       preload: path.join(__dirname, "preload.js"),
     },
     width: 800,
+    title: 'First Test App',
+    backgroundColor: "#fcba03",
+    opacity: 0.5,
+    alwaysOnTop: true,
+    // show: false,
+    // resizable:false, //drag and drop resizable
+    // movable:false,
+    fullscreen: true, //we can set fullscreeen for app ^_____^
+    frame: false,//frameless =>create a windows without tab remove frame =>splash screen ^_____^
+    // transparent: true,
   });
+
+
+  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  const childWindow = new BrowserWindow({
+    show: false,
+    frame: false,
+    transparent: true,
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+  childWindow.setBounds(mainWindow.getBounds());
+  // Show the locked overlay
+  childWindow.show();
+
+  // Load a blank page in the child window
+  childWindow.loadURL('about:blank');
+
+  // Open the DevTools for debugging
+  mainWindow.webContents.openDevTools();
+  // const child = new BrowserWindow({
+  //   width: 150,
+  //   height: 150,
+  //   parent: mainWindow,
+  //   modal: true,
+  //   show: false,
+  // });
+
+
+
+
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  // mainWindow.loadURL('https://www.digikala.com/'); //we can use url a exist website ^_____^
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.show();
+  // child.show(); //create a child tab and open after parent
+
+  // mainWindow.on('ready-to-show',function(){
+  //   mainWindow.show()
+  //   child.show()
+  // })
+
+  mainWindow.webContents.on("did-finish-load", () => {
+    dialog.showOpenDialog(mainWindow,{
+      buttonLabel:"select item",
+      defaultPath:app.getPath('desktop')
+    })
+    });
+    
+
 }
 
 // This method will be called when Electron has finished
@@ -42,3 +104,4 @@ app.on("window-all-closed", () => {
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
+
